cdb567568bcc2f25fea2f77b1f13631f0ab78728
vossca <mgerber0608@gmail.com>
2017-07-10 19:01:09 +0200

    enhancement(BasicOCR): Added JSON object creation
    Skeleton for structure_json(raw_string) function has been added, json object is constructed and returned to the main process

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
 ## ocr.py
 ## Split Bill Project
 
-import sys, json, re, numpy as np
+import sys, json, re, string, numpy as np
 import pytesseract
 from PIL import Image
 
@@ -11,15 +11,45 @@ def read_in():
 
 	return json.loads(filenames[0])
 
-#turn the raw string given into a json object
+# this function just checks whether it can parse the string it is sent to a float number
+# if it is able to parse it, it returns the value
+# if it is not able to parse it, it returns False (the string is not numeric)
+def check_if_numeric(given_string):
+	try: 
+		float(given_string)
+		return float(given_string)
+	except ValueError:
+		return False
+	
+# turn the raw string given into a json object
+# @todo clean up how the json object is constructed, add the rest of the formal jsonapi structure
+# 	identify and discard duplicate/trash items so they don't end up in the final json object
 def structure_json(raw_string):
-	return json.dumps(raw_string)
+	item_name = ""
+	json_string = '{"attributes":['
+	array_flag = False
+
+	for x in range(0, len(raw_string)):
+		return_val = check_if_numeric(raw_string[x])
+		
+		if(return_val == False):
+			if(raw_string[x] != "R"):
+				item_name += raw_string[x]
+		else:
+			if(array_flag != False):
+				json_string += ','
+			json_string += '{"desc":"' + item_name + '","price":"' + raw_string[x] + '"}'
+			array_flag = True
+			item_name = ""
+		
+	json_string += ']}'
+
+	final_dict = json.loads(json_string)
+
+	return json.dumps(final_dict)
 
 
 # main process definition, for now it is only doing basic text recognition
-# @todo call the structure_json() function and instead of printing a
-# 		string it will print a stringified json object that conforms
-# 		to the agreed upon JSON communication standard.
 
 def main():
 	filenames = read_in()
@@ -32,7 +62,9 @@ def main():
 	recognised_string = re.sub(r'[^a-zA-Z0-9\,\.\(\)]',' ',recognised_string)
 	string_list_representation = re.split(r"[\s]",recognised_string)
 
-	print(string_list_representation,'%s')
+	resulting_json = structure_json(string_list_representation);
+
+	print(resulting_json)
 
 #starting the main process 	
 if __name__ == '__main__':
