3a235d141e5e1e55784c0f0bfba72694cd18cc96
vossca <mgerber0608@gmail.com>
2017-07-14 13:01:43 +0200

    fix(BasicOCR): Python script

--- ocr.py
+++ ocr.py
@@ -1,6 +1,30 @@
+
 ## ocr.py
 ## Split Bill Project
 
+'''
+{
+	"data": {
+		"type": "success/failure",
+		"id": "xxx",
+		"attributes": {
+			"data":[
+				{"desc":"description of item 1","price":"xx.xx"},
+				{"desc":"description of item 2","price":"xx.xx"},
+				{"desc":"description of item 3","price":"xx.xx"}
+			]
+		},
+		"relationships": {
+			"data":{total":"xx.xx"}
+		}
+	},
+	"errors": {
+		"id": "xxx",
+		"code": "ERR1",
+		"title": "semantic description"
+	}
+}
+'''
 import sys, json, re, string, numpy as np
 import pytesseract
 from PIL import Image
@@ -8,19 +32,16 @@ from PIL import Image
 #get name of image(s) from stdin
 def read_in():
 	filenames = sys.stdin.readlines()
-
 	return json.loads(filenames[0])
 
-# this function just checks whether it can parse the string it is sent to a float number
-# if it is able to parse it, it returns the value
-# if it is not able to parse it, it returns False (the string is not numeric)
+#turn the raw string given into a json object
 def check_if_numeric(given_string):
 	try: 
 		float(given_string)
 		return float(given_string)
 	except ValueError:
 		return False
-	
+
 def real_item(test_string):
 	check_string1 = "total"
 	check_string2 = "vat"
@@ -56,9 +77,6 @@ def real_item(test_string):
 
 	return True
 
-# turn the raw string given into a json object
-# @todo clean up how the json object is constructed, add the rest of the formal jsonapi structure
-# 	identify and discard duplicate/trash items so they don't end up in the final json object
 def structure_json(raw_string):
 	item_name = ""
 	json_string = '{"attributes":{"data":['
@@ -91,13 +109,24 @@ def structure_json(raw_string):
 
 
 # main process definition, for now it is only doing basic text recognition
+# @todo call the structure_json() function and instead of printing a
+# 		string it will print a stringified json object that conforms
+# 		to the agreed upon JSON communication standard.
 
 def main():
 	filenames = read_in()
 	recognised_string = ""
 
 	for x in range(0, len(filenames)):
+		
+		'''
+		
+		image enhancement goes here
+
+		'''
+
 		im = Image.open(filenames[x])
+
 		recognised_string += pytesseract.image_to_string(im)
 
 	recognised_string = re.sub(r'[^a-zA-Z0-9\,\.\(\)]',' ',recognised_string)
